5805b531aeb2ce5e23ea6ebefefcc694838453c5
Alexander Alexandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-12-12 18:31:40 +0200

    Update useLocalStorage.js

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
 const useLocalStorage = (key, initialValue) => {
-  const [state, setState] = useState(() => { // func used to set a dynamic initial state and to be able to save the data in localStorage
+  const [state, setState] = useState(() => {
     try {
       let item = localStorage.getItem(key);
 
-      return item ? JSON.parse(item) : initialValue; // if the item exists, it will be saved in the localStorage, otherwise it isn't changed
+      return item ? JSON.parse(item) : initialValue;
     } catch (err) {
       console.log(err);
       return initialValue;
@@ -13,16 +13,13 @@ const useLocalStorage = (key, initialValue) => {
   });
 
   const setItem = (value) => { //the new data
-    //Todo: add support for functions
     try {
-      //save to local storage
       localStorage.setItem(key, JSON.stringify(value));
 
       setState(value);
     } catch (err) {
       console.log(err);
     }
-    //save to state
   };
 
   return [state, setItem];
